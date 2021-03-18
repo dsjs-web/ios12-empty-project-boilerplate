8b389be6db39ebb13360c6495e59739f50640038
Dimitry <dsjs.web@gmail.com>
2021-03-18 21:12:33 +0300

    initial commit

--- someApp/ViewController.swift
+++ someApp/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let r :Int = Int(UIScreen.main.bounds.size.width)/2
         // Do any additional setup after loading the view.
+        print( r-12 )
     }
 
 
